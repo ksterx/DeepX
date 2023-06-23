71e75b97bd69dd839bd17a9bedf529e687cb1314
ksterx <kostonerx@gmail.com>
2023-06-24 00:48:43 +0900

    ver0.1.1

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import find_packages, setup
 
+with open("docs/version.txt", "r") as f:
+    version = f.read().strip()
+
 setup(
     name="vision",
-    version="0.1",
+    version=version,
     packages=find_packages(),
     install_requires=[
         "torch",
---
+++ tests/test_task.py
@@ -0,0 +1,131 @@
+import lightning as L
+import torch
+from lightning.pytorch.callbacks import EarlyStopping, ModelSummary
+from lightning.pytorch.loggers import TensorBoardLogger
+from torch.utils.data import DataLoader, random_split
+from torchmetrics import Accuracy
+from torchvision import transforms
+from torchvision.datasets import CIFAR10, CIFAR100, MNIST
+
+from vision.nn.core.residual import ResNet50, ResNet101, ResNet152
+
+
+class ImageClassificationModel(L.LightningModule):
+    DATA_DIR = "./data/"
+    TRAIN_RATIO = 0.9
+    NUM_CLASSES = 100
+    NUM_CHANNELS = 3
+    DATASETS = {
+        "mnist": {"class": MNIST, "num_classes": 10, "num_channels": 1},
+        "cifar10": {"class": CIFAR10, "num_classes": 10, "num_channels": 3},
+        "cifar100": {"class": CIFAR100, "num_classes": 100, "num_channels": 3},
+    }
+    MODELS = {"resnet50": ResNet50, "resnet101": ResNet101, "resnet152": ResNet152}
+
+    def __init__(self, model_name: str, dataset_name: str, batch_size=32):
+        super().__init__()
+        self.dataset = self.DATASETS[dataset_name]
+        self.model = self.MODELS[model_name](
+            num_classes=self.dataset["num_classes"],
+            in_channels=self.dataset["num_channels"],
+        )
+        self.dataset_name = dataset_name
+        self.batch_size = batch_size
+
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        )
+
+        self.loss_fn = torch.nn.CrossEntropyLoss()
+
+        self.train_accuracy = Accuracy(task="multiclass", num_classes=self.NUM_CLASSES)
+        self.val_accuracy = Accuracy(task="multiclass", num_classes=self.NUM_CLASSES)
+        self.test_accuracy = Accuracy(task="multiclass", num_classes=self.NUM_CLASSES)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss_fn(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        self.train_accuracy.update(preds, y)
+
+        self.log("train_loss", loss)
+        self.log("train_acc", self.train_accuracy)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = torch.nn.functional.cross_entropy(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        self.val_accuracy.update(preds, y)
+
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_acc", self.val_accuracy, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = torch.nn.functional.cross_entropy(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        self.test_accuracy.update(preds, y)
+
+        self.log("test_loss", loss, prog_bar=True)
+        self.log("test_acc", self.test_accuracy, prog_bar=True)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
+
+    def prepare_data(self):
+        # download
+        self.dataset["class"](self.DATA_DIR, train=True, download=True)
+        self.dataset["class"](self.DATA_DIR, train=False, download=True)
+
+    def setup(self, stage=None):
+        # Assign train/val datasets for use in dataloaders
+        if stage == "fit" or stage is None:
+            data_full = self.dataset["class"](self.DATA_DIR, train=True, transform=self.transform)
+            len_train = int(len(data_full) * self.TRAIN_RATIO)
+            len_val = len(data_full) - len_train
+            self.data_train, self.data_val = random_split(data_full, [len_train, len_val])
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test" or stage is None:
+            self.data_test = self.dataset["class"](
+                self.DATA_DIR, train=False, transform=self.transform
+            )
+
+    def train_dataloader(self):
+        return DataLoader(self.data_train, batch_size=self.batch_size, num_workers=10)
+
+    def val_dataloader(self):
+        return DataLoader(self.data_val, batch_size=self.batch_size, num_workers=10)
+
+    def test_dataloader(self):
+        return DataLoader(self.data_test, batch_size=self.batch_size)
+
+
+def run(model_name, dataset_name, epochs=3, batch_size=32):
+    model = ImageClassificationModel(
+        model_name=model_name, dataset_name=dataset_name, batch_size=batch_size
+    )
+
+    trainer = L.Trainer(
+        max_epochs=epochs,
+        accelerator="auto",
+        logger=TensorBoardLogger(f"./experiments/{dataset_name}", name=model_name),
+        callbacks=[EarlyStopping(monitor="val_loss", patience=5), ModelSummary(max_depth=1)],
+    )
+
+    trainer.fit(model)
+    trainer.test()
+
+
+if __name__ == "__main__":
+    run("resnet50", "mnist", epochs=3, batch_size=32)
