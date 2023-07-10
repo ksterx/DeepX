3e412eb033f3e67aeebb5b53890eb1c17a1ab078
ksterx <76713276+ksterx@users.noreply.github.com>
2023-07-10 16:15:51 +0900

    fix train

--- train.py
+++ train.py
@@ -8,6 +8,9 @@ parser.add_argument("-m", "--model", type=str, nargs="*", required=True)
 parser.add_argument("-d", "--dataset", type=str, required=True)
 parser.add_argument("-b", "--batch_size", type=int, default=32)
 parser.add_argument("-e", "--epochs", type=int, default=100)
+parser.add_argument("-r", "--root_dir", type=str, default="/workspace")
+parser.add_argument("--debug", action="store_true")
+parser.add_argument("--download", action="store_true")
 args = parser.parse_args()
 
 
@@ -25,5 +28,12 @@ if isinstance(args.model, str):
     args.model = [args.model]
 
 for model in args.model:
-    trainer = trainer_cls(model, args.dataset, batch_size=args.batch_size)
+    trainer = trainer_cls(
+        model,
+        args.dataset,
+        batch_size=args.batch_size,
+        download=args.download,
+        root_dir=args.root_dir,
+        debug=args.debug,
+    )
     trainer.train(epochs=args.epochs)
