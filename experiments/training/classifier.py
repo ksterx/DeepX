93c93f8f21e13658fc6e0dda471789b5884e939e
ksterx <76713276+ksterx@users.noreply.github.com>
2023-07-25 08:43:39 +0900

    change config structure

--- experiments/training/classifier.py
+++ experiments/training/classifier.py
@@ -3,41 +3,40 @@ import hydra
 from deepx.trainers import ClassificationTrainer
 
 
-@hydra.main(
-    config_path="../conf/task/classification/", config_name="config", version_base=None
-)
+@hydra.main(config_path="../conf", config_name="config", version_base=None)
 def main(cfg):
     ds_cfg = cfg.dataset
     machine_cfg = cfg.machine
+    task_cfg = cfg.task
 
     trainer = ClassificationTrainer(
-        model=ds_cfg.model,
-        datamodule=ds_cfg.datamodule,
-        batch_size=ds_cfg.batch_size,
+        model=task_cfg.model,
+        datamodule=ds_cfg.name,
+        batch_size=task_cfg.batch_size,
         train_ratio=ds_cfg.train_ratio,
-        num_workers=ds_cfg.num_workers,
+        num_workers=machine_cfg.num_workers,
         download=ds_cfg.download,
-        lr=ds_cfg.lr,
-        beta1=ds_cfg.beta1,
-        beta2=ds_cfg.beta2,
-        loss_fn=ds_cfg.loss_fn,
-        optimizer=ds_cfg.optimizer,
-        scheduler=ds_cfg.scheduler,
+        lr=task_cfg.lr,
+        beta1=task_cfg.beta1,
+        beta2=task_cfg.beta2,
+        loss_fn=task_cfg.loss_fn,
+        optimizer=task_cfg.optimizer,
+        scheduler=task_cfg.scheduler,
         root_dir=machine_cfg.root_dir,
         data_dir=machine_cfg.data_dir,
         log_dir=machine_cfg.log_dir,
-        dropout=ds_cfg.dropout,
+        dropout=task_cfg.dropout,
     )
     trainer.train(
-        ckpt_path=ds_cfg.ckpt_path,
-        epochs=ds_cfg.epochs,
-        stopping_patience=ds_cfg.patience,
-        max_depth=ds_cfg.max_depth,
-        benchmark=ds_cfg.benchmark,
-        debug=ds_cfg.debug,
-        monitor=ds_cfg.monitor,
-        monitor_max=ds_cfg.monitor_max,
-        logger=ds_cfg.logger,
+        ckpt_path=task_cfg.ckpt_path,
+        epochs=task_cfg.epochs,
+        stopping_patience=task_cfg.patience,
+        max_depth=task_cfg.max_depth,
+        benchmark=task_cfg.benchmark,
+        debug=task_cfg.debug,
+        monitor=task_cfg.monitor,
+        monitor_max=task_cfg.monitor_max,
+        logger=task_cfg.logger,
     )
 
 
--- experiments/training/imggenerator.py
+++ experiments/training/imggenerator.py
@@ -4,46 +4,47 @@ from deepx.trainers import ImageGenerationTrainer
 
 
 @hydra.main(
-    config_path="../conf/task/image_generation/",
+    config_path="../conf",
     config_name="config",
     version_base=None,
 )
 def main(cfg):
     ds_cfg = cfg.dataset
     machine_cfg = cfg.machine
+    task_cfg = cfg.task
 
     trainer = ImageGenerationTrainer(
-        model=ds_cfg.model,
-        datamodule=ds_cfg.datamodule,
-        batch_size=ds_cfg.batch_size,
+        model=task_cfg.model,
+        datamodule=ds_cfg.name,
+        batch_size=task_cfg.batch_size,
         train_ratio=ds_cfg.train_ratio,
-        num_workers=ds_cfg.num_workers,
+        num_workers=machine_cfg.num_workers,
         download=ds_cfg.download,
-        lr=ds_cfg.lr,
-        beta1=ds_cfg.beta1,
-        beta2=ds_cfg.beta2,
-        loss_fn=ds_cfg.loss_fn,
-        optimizer=ds_cfg.optimizer,
-        scheduler=ds_cfg.scheduler,
+        lr=task_cfg.lr,
+        beta1=task_cfg.beta1,
+        beta2=task_cfg.beta2,
+        loss_fn=task_cfg.loss_fn,
+        optimizer=task_cfg.optimizer,
+        scheduler=task_cfg.scheduler,
         root_dir=machine_cfg.root_dir,
         data_dir=machine_cfg.data_dir,
         log_dir=machine_cfg.log_dir,
-        negative_slope=ds_cfg.negative_slope,
-        dropout=ds_cfg.dropout,
-        latent_dim=ds_cfg.latent_dim,
-        base_dim_g=ds_cfg.base_dim_g,
-        base_dim_d=ds_cfg.base_dim_d,
+        negative_slope=task_cfg.negative_slope,
+        dropout=task_cfg.dropout,
+        latent_dim=task_cfg.latent_dim,
+        base_dim_g=task_cfg.base_dim_g,
+        base_dim_d=task_cfg.base_dim_d,
     )
     trainer.train(
-        ckpt_path=ds_cfg.ckpt_path,
-        epochs=ds_cfg.epochs,
-        stopping_patience=ds_cfg.patience,
-        max_depth=ds_cfg.max_depth,
-        benchmark=ds_cfg.benchmark,
-        debug=ds_cfg.debug,
-        monitor=ds_cfg.monitor,
-        monitor_max=ds_cfg.monitor_max,
-        logger=ds_cfg.logger,
+        ckpt_path=task_cfg.ckpt_path,
+        epochs=task_cfg.epochs,
+        stopping_patience=task_cfg.patience,
+        max_depth=task_cfg.max_depth,
+        benchmark=task_cfg.benchmark,
+        debug=task_cfg.debug,
+        monitor=task_cfg.monitor,
+        monitor_max=task_cfg.monitor_max,
+        logger=task_cfg.logger,
     )
 
 
